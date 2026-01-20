c0959ad746d409b56b2631841d3225e54ed7729f
Alex_Lushenko <mralealex9@gmail.com>
2026-01-20 22:51:29 +0200

    Solution

--- services/user.py
+++ services/user.py
@@ -15,8 +15,8 @@ def create_user(
         username=username,
         password=password,
         email=email,
-        first_name=first_name,
-        last_name=last_name
+        first_name=first_name or "",
+        last_name=last_name or ""
     )
 
 
