28c31d53b23319b612517d5ee9c7b3537f1f3e64
Alex_Lushenko <mralealex9@gmail.com>
2026-01-21 21:57:03 +0200

    Fixed Solution

--- db/models.py
+++ db/models.py
@@ -72,7 +72,8 @@ class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(
         to=settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        related_name="orders"
     )
 
     class Meta:
@@ -85,7 +86,8 @@ class Order(models.Model):
 class Ticket(models.Model):
     movie_session = models.ForeignKey(
         to=MovieSession,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        related_name="tickets"
     )
     order = models.ForeignKey(
         to=Order,
@@ -122,7 +124,11 @@ class Ticket(models.Model):
         if errors:
             raise ValidationError(errors)
 
-    def save(self, *args, **kwargs) -> None:
+    def save(
+        self,
+        *args,
+        **kwargs
+    ) -> None:
         self.full_clean()
         return super().save(*args, **kwargs)
 
